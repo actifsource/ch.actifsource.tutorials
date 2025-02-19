de21e9f353ee08c6cfe7d7e79f11b4e99bf85f5f
Dominic Meier <dominic.meier@mides.ch>
2025-02-19 14:35:32 +0100

    testapp aufgeräumt

---
+++ ch.actifsource.tutorial.cip.test/src-gen/ch/actifsource/tutorial/cip/test/TestPackage.java
@@ -0,0 +1,26 @@
+package ch.actifsource.tutorial.cip.test;
+
+import ch.actifsource.core.Resource;
+
+public class TestPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private TestPackage() {}
+  
+  public static final String getScopeName() {
+    return "ch.actifsource.tutorial.cip.test";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "ch.actifsource.tutorial.cip.test";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,ch.actifsource.tutorial.cip.test,6NCbzgIlA7IUeI6r7L5qnuptGhI=] */
