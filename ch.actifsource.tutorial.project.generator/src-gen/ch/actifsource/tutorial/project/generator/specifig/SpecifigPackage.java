8263ac1a57917e4c939e14ec3eb0f88fe88793cd
Dominic Meier <dominic.meier@mides.ch>
2025-02-21 12:02:34 +0100

    #4 Fix pathnames porject generator

---
+++ ch.actifsource.tutorial.project.generator/src-gen/ch/actifsource/tutorial/project/generator/specifig/SpecifigPackage.java
@@ -0,0 +1,28 @@
+package ch.actifsource.tutorial.project.generator.specifig;
+
+import ch.actifsource.core.Resource;
+
+public class SpecifigPackage {
+  
+  public static final Resource Project = new Resource("f105eaa7-f03e-11ef-9045-cbf3500551a0");
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private SpecifigPackage() {}
+  
+  public static final String getScopeName() {
+    return "ch.actifsource.tutorial.project.generator";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "ch.actifsource.tutorial.project.generator.specifig";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,ch.actifsource.tutorial.project.generator.specifig,ILZ76n81/BHbH0FZhlB2jG2/314=] */
---
+++ ch.actifsource.tutorial.project.generator/src-gen/ch/actifsource/tutorial/project/generator/specifig/javamodel/IProject.java
@@ -0,0 +1,11 @@
+package ch.actifsource.tutorial.project.generator.specifig.javamodel;
+
+import ch.actifsource.util.collection.IMultiMapOrdered;
+
+public interface IProject extends ch.actifsource.core.javamodel.INamedResource {
+
+  public static final ch.actifsource.core.INode TYPE_ID = new ch.actifsource.core.Resource("f105eaa7-f03e-11ef-9045-cbf3500551a0");
+  
+}
+
+/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,f105eaa7-f03e-11ef-9045-cbf3500551a0,6DTD6RxRoCSSc8C9abNFth6ZU6U=] */
